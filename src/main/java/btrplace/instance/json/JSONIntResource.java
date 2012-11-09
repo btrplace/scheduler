11f335a811225eca0918eb4a202af22c2dd3df6f
Fabien Hermenier <hermenierfabien@gmail.com>
2012-11-09 16:13:05 +0100

    improve resource serialization/unserialization

--- src/main/java/btrplace/instance/json/JSONIntResource.java
+++ src/main/java/btrplace/instance/json/JSONIntResource.java
@@ -1,9 +1,14 @@
 package btrplace.instance.json;
 
+import btrplace.instance.DefaultIntResource;
 import btrplace.instance.IntResource;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Set;
 import java.util.UUID;
 
@@ -13,7 +18,30 @@ import java.util.UUID;
  */
 public class JSONIntResource {
 
+    private JSONParser p;
+
+    /**
+     * New instance.
+     */
+    public JSONIntResource() {
+        p = new JSONParser();
+    }
+
+    /**
+     * Serialize a resource to a String.
+     * @param rc the resource to serialize
+     * @return the resulting String
+     */
     public String toJSON(IntResource rc) {
+        return toJSONObject(rc).toString();
+    }
+
+    /**
+     * Convert to a JSONObject
+     * @param rc the resource to serialize
+     * @return the resulting object
+     */
+    public JSONObject toJSONObject(IntResource rc) {
         JSONObject o = new JSONObject();
         o.put("id", rc.identifier());
         Set<UUID> elems = rc.getDefined();
@@ -22,10 +50,46 @@ public class JSONIntResource {
             values.put(u, rc.get(u));
         }
         o.put("values", values);
-        return o.toString();
+        return o;
     }
 
-    public IntResource fromJSON(String s) {
-        return null;
+    /**
+     * Parse a JSON message to get a resource from a stream.
+     * @param r the stream.
+     * @return the resulting resource or {@code null} in case of error
+     * @throws IOException if an error occurred while reading the stream
+     */
+    public IntResource fromJSON(Reader r) throws IOException {
+        try {
+            JSONObject o = (JSONObject) p.parse(r);
+            if(!o.containsKey("id") || !o.containsKey("values")) {
+                return null;
+            }
+            IntResource rc = new DefaultIntResource((String)o.get("id"));
+            JSONObject values = (JSONObject) o.get("values");
+            for (Object k : values.keySet()) {
+                UUID u = UUID.fromString(k.toString());
+                int v = Integer.parseInt(values.get(k).toString());
+                rc.set(u, v);
+            }
+            return rc;
+        } catch( ParseException ex) {
+            return null;
+        } catch (ClassCastException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Parse a JSON String to get a resource.
+     * @param s the string to parse
+     * @return the resulting resource or {@code null} in case of error
+     */
+    public IntResource fromJSON(String s)  {
+        try {
+            return fromJSON(new StringReader(s));
+        } catch (IOException ex) {
+            return null;
+        }
     }
 }
--- src/test/java/btrplace/instance/json/JSONIntResourceTest.java
+++ src/test/java/btrplace/instance/json/JSONIntResourceTest.java
@@ -13,13 +13,18 @@ import java.util.UUID;
 public class JSONIntResourceTest {
 
     @Test
-    public void testSerialize() {
+    public void testSimple() {
         IntResource rc = new DefaultIntResource("foo");
         rc.set(UUID.randomUUID(), 3);
         rc.set(UUID.randomUUID(), 4);
         rc.set(UUID.randomUUID(), 5);
         rc.set(UUID.randomUUID(), 6);
         JSONIntResource s = new JSONIntResource();
+        String str = s.toJSON(rc);
+        IntResource rc2 = s.fromJSON(str);
         System.out.println(s.toJSON(rc));
+        System.out.println(str);
+        System.out.println(rc2);
+
     }
 }
