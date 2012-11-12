7561e61bd32a1da318e0a1cd46047327ffcc1ac8
Fabien Hermenier <hermenierfabien@gmail.com>
2012-11-12 08:40:52 +0100

    omitted file

---
+++ src/main/java/btrplace/plan/Action.java
@@ -0,0 +1,33 @@
+package btrplace.plan;
+
+import btrplace.model.Model;
+
+/**
+ * An action to perform on an element and that will alter an instance on success.
+ *
+ * @author Fabien Hermenier
+ */
+public interface Action {
+
+    /**
+     * Apply the action on an instance.
+     *
+     * @param i the instance to alter with the action
+     * @return {@code true} if the action was applied successfully
+     */
+    boolean apply(Model i);
+
+    /**
+     * Get the moment the action starts.
+     *
+     * @return a positive integer
+     */
+    int getStart();
+
+    /**
+     * Get the moment the action ends.
+     *
+     * @return a positive integer
+     */
+    int getEnd();
+}
