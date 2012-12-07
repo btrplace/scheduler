0fbd4e6b35b6bb7a53557c2b56f645f5da62c0b1
Fabien Hermenier <hermenierfabien@gmail.com>
2012-12-07 12:51:56 +0100

    unit test for CRoot.

---
+++ src/test/java/btrplace/solver/choco/constraint/CRootTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2012 University of Nice Sophia-Antipolis
+ *
+ * This file is part of btrplace.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package btrplace.solver.choco.constraint;
+
+import btrplace.model.*;
+import btrplace.model.constraint.Online;
+import btrplace.model.constraint.Root;
+import btrplace.plan.ReconfigurationPlan;
+import btrplace.solver.SolverException;
+import btrplace.solver.choco.ChocoReconfigurationAlgorithm;
+import btrplace.solver.choco.DefaultChocoReconfigurationAlgorithm;
+import junit.framework.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Unit tests for {@link CRoot}.
+ *
+ * @author Fabien Hermenier
+ */
+public class CRootTest {
+
+    @Test
+    public void testBasic() throws SolverException {
+        Mapping map = new DefaultMapping();
+        UUID n1 = UUID.randomUUID();
+        UUID n2 = UUID.randomUUID();
+        UUID vm1 = UUID.randomUUID();
+        UUID vm2 = UUID.randomUUID();
+        UUID vm3 = UUID.randomUUID();
+        map.addOnlineNode(n1);
+        map.addOnlineNode(n2);
+        map.addRunningVM(vm1, n1);
+        map.addSleepingVM(vm2, n1);
+        map.addReadyVM(vm3);
+
+        Model mo = new DefaultModel(map);
+
+        ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
+        Root r1 = new Root(map.getAllVMs());
+        Online n = new Online(map.getAllNodes());
+        List<SatConstraint> l = new ArrayList<SatConstraint>();
+        l.add(r1);
+        l.add(n);
+        ReconfigurationPlan p = cra.solve(mo, l);
+        Assert.assertNotNull(p);
+        Model res = p.getResult();
+        Assert.assertEquals(n1, res.getMapping().getVMLocation(vm1));
+    }
+}
