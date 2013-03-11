9b051309ac1effb7b39edf982494f51227efe0b3
Fabien Hermenier <hermenierfabien@gmail.com>
2013-03-11 14:33:32 +0100

    Refactor the tutorials

--- samples/src/main/bin/btrplace-tut.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Usage: $0 [run|reset] tutorial_name"
-echo "\trun: compile and run the expected tutorial"
-echo "\treset: delete all modifications made on the tutorial"
-echo "Available tutorials: "
-
--- samples/src/main/java/btrplace/samples/Sample.java
+++
@@ -1,9 +0,0 @@
-package btrplace.samples;
-
-/**
- * @author Fabien Hermenier
- */
-public interface Sample {
-
-    boolean run() throws Exception;
-}
