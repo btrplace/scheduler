0faf97798a3e17335365cc096d90adabb270ed8e
Fabien Hermenier <hermenierfabien@gmail.com>
2013-05-24 16:40:14 +0200

    New tutorial about model customization

---
+++ examples/src/main/java/btrplace/examples/ModelCustomization.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2013 University of Nice Sophia-Antipolis
+ *
+ * This file is part of btrplace.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package btrplace.examples;
+
+import btrplace.model.*;
+import btrplace.model.constraint.*;
+import btrplace.model.view.ShareableResource;
+import btrplace.plan.ReconfigurationPlan;
+import btrplace.plan.event.ForgeVM;
+import btrplace.plan.event.MigrateVM;
+import btrplace.solver.SolverException;
+import btrplace.solver.choco.ChocoReconfigurationAlgorithm;
+import btrplace.solver.choco.DefaultChocoReconfigurationAlgorithm;
+import btrplace.solver.choco.durationEvaluator.DurationEvaluator;
+import btrplace.solver.choco.durationEvaluator.DurationEvaluators;
+
+import java.util.*;
+
+/**
+ * Tutorial about the basic tuning of a model.
+ * The document associated to the tutorial is available
+ * on <a href="https://github.com/fhermeni/btrplace-solver/wiki/Customizing-a-model">btrplace website</a>.
+ *
+ * @author Fabien Hermenier
+ */
+public class ModelCustomization implements Example {
+
+    private UUID n1 = new UUID(1, 1);
+    private UUID n2 = new UUID(1, 2);
+    private UUID vm1 = new UUID(2, 1);
+    private UUID vm2 = new UUID(2, 2);
+    private UUID vm3 = new UUID(2, 3);
+    private UUID vm4 = new UUID(2, 4);
+    private UUID vm5 = new UUID(2, 5);
+    private UUID vm6 = new UUID(2, 6);
+    private UUID vm7 = new UUID(2, 7);
+    private UUID vm8 = new UUID(2, 8);
+    private UUID vm9 = new UUID(2, 9);
+    private UUID vm10 = new UUID(2, 10);
+
+    private Set<UUID> g1 = new HashSet<>(Arrays.asList(vm1, vm2, vm4));
+    private Set<UUID> g2 = new HashSet<>(Arrays.asList(vm5, vm6, vm8));
+
+    /**
+     * We customize the estimate duration of the VM migration action
+     * to be equals to 2 second per GB of memory plus 3 seconds
+     */
+    class MyMigrationEvaluator implements DurationEvaluator {
+
+        ShareableResource rc;
+
+        MyMigrationEvaluator(ShareableResource rcMem) {
+            rc = rcMem;
+        }
+
+        @Override
+        public int evaluate(UUID e) {
+            return rc.get(e) * 2 + 3;
+        }
+
+    }
+
+    /**
+     * We customize the estimate duration of a ForgeVM action
+     * to be equals to 2 or 5 seconds, depending on the VM template
+     */
+    class MyForgeEvaluator implements DurationEvaluator {
+        Attributes attrs;
+
+        MyForgeEvaluator(Attributes a) {
+            attrs = a;
+        }
+
+        @Override
+        public int evaluate(UUID e) {
+            String tpl = attrs.isSet(e, "template") ? attrs.getString(e, "template") : "default";
+            switch (tpl) {
+                case "small":
+                    return 2;
+                case "large":
+                    return 6;
+                default:
+                    return 60;
+            }
+        }
+    }
+
+    private Model makeModel() {
+
+        Mapping map = new DefaultMapping();
+
+        map.addOnlineNode(n1);
+        map.addOnlineNode(n2);
+
+        ShareableResource rcMem = new ShareableResource("mem", 32); //32GB by default
+
+        rcMem.set(vm1, 1).set(vm2, 2).set(vm3, 3)
+                .set(vm4, 1).set(vm5, 2).set(vm6, 3)
+                .set(vm7, 1).set(vm8, 2).set(vm9, 3).set(vm10, 1);
+
+        map.addRunningVM(vm1, n1);
+        map.addRunningVM(vm2, n1);
+        map.addRunningVM(vm3, n1);
+        map.addRunningVM(vm4, n1);
+        map.addRunningVM(vm5, n1);
+        map.addRunningVM(vm6, n1);
+        map.addRunningVM(vm7, n2);
+        map.addRunningVM(vm8, n2);
+        map.addRunningVM(vm9, n2);
+        map.addReadyVM(vm10);
+
+        Model mo = new DefaultModel(map);
+        mo.attach(rcMem);
+        return mo;
+    }
+
+    @Override
+    public boolean run() {
+
+        Model mo = makeModel();
+
+        //Set the duration evaluators
+        ChocoReconfigurationAlgorithm cra = new DefaultChocoReconfigurationAlgorithm();
+        DurationEvaluators dev = cra.getDurationEvaluators();
+        dev.register(MigrateVM.class,
+                new MyMigrationEvaluator((ShareableResource) mo.getView(ShareableResource.VIEW_ID_BASE + "mem")));
+        dev.register(ForgeVM.class, new MyForgeEvaluator(mo.getAttributes()));
+
+
+        //Set some attributes
+        Attributes attrs = mo.getAttributes();
+        for (UUID vm : mo.getMapping().getAllVMs()) {
+            long i = vm.getLeastSignificantBits();
+            attrs.put(vm, "template", i % 2 == 0 ? "small" : "large");
+            attrs.put(vm, "clone", true);
+        }
+        List<SatConstraint> cstrs = new ArrayList<>();
+
+        //No more than 5 VMs per node
+        cstrs.add(new SingleRunningCapacity(mo.getMapping().getAllNodes(), 5));
+
+        //vm1 and vm10 on the same node
+        cstrs.add(new Gather(new HashSet<>(Arrays.asList(vm1, vm10))));
+
+        //(vm1, vm2, vm4) and (vm5, vm6, vm8) must not share node
+        cstrs.add(new Split(new HashSet<>(Arrays.asList(g1, g2))));
+
+        //vm10 must be running
+        cstrs.add(new Running(Collections.singleton(vm10)));
+
+        try {
+            cra.doOptimize(true);
+            ReconfigurationPlan plan = cra.solve(mo, cstrs);
+            System.out.println(plan);
+        } catch (SolverException ex) {
+            System.err.println(ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
--- examples/src/test/java/btrplace/ExamplesIntegration.java
+++ examples/src/test/java/btrplace/ExamplesIntegration.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright (c) 2013 University of Nice Sophia-Antipolis
+ *
+ * This file is part of btrplace.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package btrplace;
 
 import btrplace.examples.BasicTuning;
 import btrplace.examples.Example;
 import btrplace.examples.GettingStarted;
+import btrplace.examples.ModelCustomization;
 import junit.framework.Assert;
 import org.testng.annotations.Test;
 
@@ -22,6 +40,12 @@ public class ExamplesIntegration {
     @Test
     public void testBasicTuning() throws Exception {
         Example ex = new BasicTuning();
+        Assert.assertTrue("Example " + ex.toString() + " failed", ex.run());
+    }
+
+    @Test
+    public void testModelCustomization() throws Exception {
+        Example ex = new ModelCustomization();
         Assert.assertTrue("Example " + ex.toString() + " failed", ex.run());
     }
 
