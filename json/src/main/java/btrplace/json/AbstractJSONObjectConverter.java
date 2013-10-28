08b6f64d03a11abb1ca2c779f00bd6c01d609935
Fabien Hermenier <hermenierfabien@gmail.com>
2013-10-28 16:25:36 +0100

    Fix JSON un-serialisation that forgot to declare elements.

--- json/src/main/java/btrplace/json/AbstractJSONObjectConverter.java
+++ json/src/main/java/btrplace/json/AbstractJSONObjectConverter.java
@@ -329,7 +329,9 @@ public abstract class AbstractJSONObjectConverter<E> implements JSONObjectConver
         if (mo == null) {
             throw new JSONConverterException("Unable to extract VMs without a model to use as a reference");
         }
-        return new VM(vmID);
+        VM v = new VM(vmID);
+        mo.newVM(vmID);
+        return v;
     }
 
     /**
@@ -345,7 +347,9 @@ public abstract class AbstractJSONObjectConverter<E> implements JSONObjectConver
         if (mo == null) {
             throw new JSONConverterException("Unable to extract VMs without a model to use as a reference");
         }
-        return new Node(nodeID);
+        mo.newNode(nodeID);
+        Node n = new Node(nodeID);
+        return n;
     }
 
     @Override
