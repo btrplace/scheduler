486aa27f5ed18b62eb9d6cbdddd9f1a576cb4c55
Fabien Hermenier <hermenierfabien@gmail.com>
2013-12-24 10:46:42 +0100

    Unit tests

--- safeplace/src/main/java/btrplace/solver/api/cstrSpec/invariant/Minus.java
+++ safeplace/src/main/java/btrplace/solver/api/cstrSpec/invariant/Minus.java
@@ -15,6 +15,9 @@ public class Minus implements Term {
     private Term a, b;
 
     public Minus(Term t1, Term t2) {
+        if (!t1.type().equals(t2.type())) {
+            throw new RuntimeException("Unconsistent substraction " + t1.type() + " + " + t2.type());
+        }
         this.a = t1;
         this.b = t2;
     }
@@ -39,7 +42,7 @@ public class Minus implements Term {
             }
             return l;
         }
-        throw new RuntimeException("Unsupported operation on '" + o1.getClass().getSimpleName() + "'");
+        throw new RuntimeException("Unsupported operation on '" + a.type() + "'");
     }
 
     @Override
---
+++ safeplace/src/test/java/btrplace/solver/api/cstrSpec/invariant/MinusTest.java
@@ -0,0 +1,50 @@
+package btrplace.solver.api.cstrSpec.invariant;
+
+import btrplace.model.DefaultModel;
+import btrplace.solver.api.cstrSpec.invariant.type.NatType;
+import btrplace.solver.api.cstrSpec.invariant.type.SetType;
+import btrplace.solver.api.cstrSpec.invariant.type.VMStateType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Set;
+
+/**
+ * @author Fabien Hermenier
+ */
+public class MinusTest {
+
+    @Test
+    public void testInts() {
+        Minus p = new Minus(NatType.getInstance().newValue(5), NatType.getInstance().newValue(7));
+        Assert.assertEquals(p.getValue(new DefaultModel()), -2);
+        Assert.assertEquals(p.type(), NatType.getInstance());
+    }
+
+    @Test
+    public void testCollections() {
+        Value v1 = new Value(Arrays.asList(1, 2), new SetType(NatType.getInstance()));
+        Value v2 = new Value(Arrays.asList(2, 5), new SetType(NatType.getInstance()));
+        Minus p = new Minus(v1, v2);
+        Set s = (Set) p.getValue(new DefaultModel());
+        Assert.assertEquals(s.size(), 1);
+        Assert.assertEquals(p.type(), new SetType(NatType.getInstance()));
+    }
+
+    @Test(expectedExceptions = {RuntimeException.class})
+    public void testBadCollections() throws RuntimeException {
+        Value v1 = new Value(Arrays.asList(1, 2), new SetType(NatType.getInstance()));
+        Value v2 = new Value(Arrays.asList(VMStateType.getInstance().newValue("running")), new SetType(VMStateType.getInstance()));
+        new Minus(v1, v2);
+    }
+
+    @Test
+    public void testMinusMinus() {
+        Minus p1 = new Minus(NatType.getInstance().newValue(5), NatType.getInstance().newValue(7));
+        Minus p2 = new Minus(NatType.getInstance().newValue(1), NatType.getInstance().newValue(2));
+        Minus p3 = new Minus(p1, p2);
+        Assert.assertEquals(p3.getValue(new DefaultModel()), -1);
+    }
+
+}
