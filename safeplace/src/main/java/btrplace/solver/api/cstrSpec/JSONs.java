3b886fa3ab821cc5d1f25d2e40418744c9747f46
Fabien Hermenier <hermenierfabien@gmail.com>
2014-01-06 11:09:59 +0100

    better unmarshaling

--- safeplace/src/main/java/btrplace/solver/api/cstrSpec/JSONs.java
+++ safeplace/src/main/java/btrplace/solver/api/cstrSpec/JSONs.java
@@ -13,22 +13,31 @@ public class JSONs {
 
     public static String unMarshal(String json, Map<String, Object> params) {
         for (Map.Entry<String, Object> e : params.entrySet()) {
-            Object v = e.getValue();
-            String s = e.getValue().toString();
-            if (v instanceof Element) {
-                s = Integer.toString(((Element) v).id());
-            } else if (v instanceof Collection) {
-                StringBuilder str = new StringBuilder("[");
-                Iterator<Object> ite = ((Collection) v).iterator();
-                str.append(((Element) ite.next()).id());
-                while (ite.hasNext()) {
-                    str.append(", ");
-                    str.append(((Element) ite.next()).id());
-                }
-                s = str.append("]").toString();
-            }
-            json = json.replaceAll("@" + e.getKey(), s);
+            json = json.replaceAll("@" + e.getKey(), unMarshal(e.getValue()));
         }
         return json;
+    }
+
+    public static String unMarshal(Object v) {
+        String s;
+        if (v instanceof Element) {
+            s = Integer.toString(((Element) v).id());
+        } else if (v instanceof Collection) {
+            StringBuilder str = new StringBuilder("[");
+            Iterator<Object> ite = ((Collection) v).iterator();
+            str.append(unMarshal(ite.next()));
+            while (ite.hasNext()) {
+                str.append(", ");
+                str.append(unMarshal(ite.next()));
+            }
+            s = str.append("]").toString();
+        } else if (v instanceof Boolean) {
+            s = v.equals(Boolean.TRUE) ? "true" : "false";
+        } else if (v instanceof String || v instanceof Number) {
+            s = v.toString();
+        } else {
+            throw new IllegalArgumentException("No serialisation available for value '" + v + "' (" + v.getClass().getSimpleName() + ")");
+        }
+        return s;
     }
 }
