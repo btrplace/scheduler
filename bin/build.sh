dafc6f351dfcfe7c7e94d063dc865a5f2a85de5c
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-06 14:13:33 +0200

    Fix scripting

--- bin/build.sh
+++ bin/build.sh
@@ -1,26 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-function getVersionToRelease {
+function getVersionToRelease() {
     CURRENT_VERSION=`mvn ${MVN_ARGS} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[INFO\]"`
     echo ${CURRENT_VERSION%%-SNAPSHOT}
 }
 
-if [ ${TRAVIS_BRANCH} =~ ^release ]; then
+if test -z "$TRAVIS_BRANCH"; then
+    BRANCH=$(git rev-parse --abbrev-ref HEAD)
+else
+    BRANCH=${TRAVIS_BRANCH}
+fi
+
+if [ ${BRANCH} = "release" ]; then
     #Extract the version
     VERSION=getVersionToRelease
     TAG="btrplace-scheduler-${VERSION}"
 
     #Quit if tag already exists
-    git ls-remote --exit-code --tags origin ${TAG} || exit 1
+    git ls-remote --exit-code --tags origin ${TAG} ||exit 1
 
     #Establish the version, maven side, misc. side
     ./bin/set_version.sh ${VERSION} || exit 1
 
     #Working version ?
-    mvn clean test || exit 1
+    mvn clean test ||exit 1
 
     #Tag the code
-    git tag ${TAG} || exit 1
+    git tag ${TAG} ||exit 1
 
     #Integrate with master
     git checkout master
@@ -33,8 +39,8 @@ if [ ${TRAVIS_BRANCH} =~ ^release ]; then
     git commit -m "Prepare the code for the next version" -a
 
     #Push changes, with the tag
-    git push origin master develop || exit 1
-    git push --tags || exit 1
+    git push origin master develop ||exit 1
+    git push --tags ||exit 1
 
     #Deploy the artifacts
     mvn deploy || exit 1
