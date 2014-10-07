4bb22152c4c3862bbb3f4a01b0c87e89679e9d5c
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-07 16:04:14 +0200

    remove a key

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gpg
-gpg --import etc/private.key
+gpg --allow-secret-key-import --import etc/private.key
 mvn -s etc/sonatype.xml clean javadoc:jar source:jar gpg:sign -Dgpg.keyname=BF1447AC -Dgpg.passphrase=${GPG_PASSPHRASE} deploy ||exit 1
