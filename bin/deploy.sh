07ee6b015579c96e8dd6450e01787340deed4fe9
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-07 15:27:44 +0200

    stuff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gpg --import etc/private.key
-mvn -s etc/sonatype.xml clean javadoc:jar source:jar gpg:sign -Dgpg.passphrase="${GPG_PASSPHRASE}" deploy ||exit 1
+echo ${GPG_PASSPHRASE}
+mvn -s etc/sonatype.xml clean javadoc:jar source:jar gpg:sign -Dgpg.passphrase=${GPG_PASSPHRASE} deploy ||exit 1
