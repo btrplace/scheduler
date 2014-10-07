674df68f4d6f744026e28a9f2499f5d396f2b891
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-07 15:53:36 +0200

    stuff

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+gpg
 gpg --import etc/private.key
 mvn -s etc/sonatype.xml clean javadoc:jar source:jar gpg:sign -Dgpg.keyname=BF1447AC -Dgpg.passphrase=${GPG_PASSPHRASE} deploy ||exit 1
