81cd247899f6c403a43be15e31396583184b4511
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-10 11:34:44 +0200

    guard on release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+#Shell script to initiate the release
+#The script creates a 'release' branch, set the version the pom.xml and the changelog then push
+#The remainder of the process should be handled by the build_release.sh script.
 
 function getVersionToRelease() {
     CURRENT_VERSION=`mvn ${MVN_ARGS} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v "\[INFO\]"`
     echo ${CURRENT_VERSION%%-SNAPSHOT}
 }
 
+git ls-remote --exit-code --heads origin release > /dev/null
+if [ $? -eq 0 ]; then
+    echo "Error: A release is already under progress"
+    exit 1
+fi
+
+echo "here"
+exit
 VERSION=$(getVersionToRelease)
 git checkout -b release || exit 1
 
