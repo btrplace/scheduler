b57d2d86d02be0fe5c822d6ea3777a5c4c38fec9
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-12 00:15:31 +0200

    - make the script work on a travis instance and local machine - sonatype repositories

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gpg --allow-secret-key-import --import etc/private.key
-mvn -s etc/sonatype.xml clean javadoc:jar source:jar gpg:sign -Dgpg.keyname=BF1447AC -Dgpg.passphrase=${GPG_PASSPHRASE} deploy ||exit 1
+if [ -e etc/private.key ]; then
+	gpg --allow-secret-key-import --import etc/private.key
+	PASSPHRASE="-Dgpg.passphrase=${GPG_PASSPHRASE}"
+	SETTINGS="-s etc/sonatype.xml"
+fi
+mvn ${SETTINGS} clean javadoc:jar source:jar gpg:sign -Dgpg.keyname=BF1447AC ${PASSPHRASE}  deploy ||exit 1
