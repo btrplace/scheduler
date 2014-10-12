ceb3e3d0eec3b745a20a9a198c83d26fa36b93c7
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-12 21:57:30 +0200

    missing developer information (required by sonatype)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,6 @@
 if [ -e etc/private.key ]; then
 	gpg --allow-secret-key-import --import etc/private.key
 	PASSPHRASE="-Dgpg.passphrase=${GPG_PASSPHRASE}"
-	SETTINGS="-s etc/sonatype.xml"
+	SETTINGS="-q -s etc/sonatype.xml"
 fi
-mvn ${SETTINGS} -q clean javadoc:jar source:jar -Dgpg.keyname=BF1447AC ${PASSPHRASE} deploy ||exit 1
+mvn ${SETTINGS} clean javadoc:jar source:jar -Dgpg.keyname=BF1447AC ${PASSPHRASE} deploy ||exit 1
