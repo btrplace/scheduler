03f7cb899cd7c61bc971afb2b9ffcb55ebfa07b6
Fabien Hermenier <fabien.hermenier@unice.fr>
2014-10-13 08:27:58 +0200

    a guard to prevent to make a release by mistake

--- bin/build_snapshot.sh
+++ bin/build_snapshot.sh
@@ -5,5 +5,15 @@ function quit() {
     exit 1
 }
 
-mvn clean test||exit 1
-./bin/deploy.sh||quit "Unable to deploy"
+function getVersion() {
+    mvn ${MVN_ARGS} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version |grep "^[0-9]\+\\.[0-9]\+" 2>/dev/null
+}
+
+V=$(getVersion)
+if [[ ${V} == *-SNAPSHOT ]]; then
+    mvn clean test||exit 1
+    ./bin/deploy.sh||quit "Unable to deploy"
+else
+    echo "${V} is not a snapshot version. Exiting"
+    exit 1
+fi
