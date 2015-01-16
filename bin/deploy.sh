7ef57a5fba1f7e85bf44c33522c71109c01ad768
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-16 12:46:44 +0100

    reduce deploy verbosity

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,5 @@ if [ -e etc/gpg ]; then
 	PASSPHRASE="-Dgpg.passphrase=${GPG_PASSPHRASE}"
 	SETTINGS="-s etc/sonatype.xml"
 fi
-mvn ${SETTINGS} clean javadoc:jar source:jar -Dgpg.keyname=BF1447AC ${PASSPHRASE} deploy ||exit 1
+mvn ${SETTINGS} clean javadoc:jar source:jar -Dgpg.keyname=BF1447AC ${PASSPHRASE} deploy |grep -v "^Generating"
+exit $?
