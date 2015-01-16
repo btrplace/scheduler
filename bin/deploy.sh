904f067065d8b29f9654c9ee10c6008229964b7a
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-16 23:23:00 +0100

    verbosity++

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@ if [ -e etc/gpg ]; then
 	PASSPHRASE="-Dgpg.passphrase=${GPG_PASSPHRASE}"
 	SETTINGS="-s etc/sonatype.xml"
 fi
-mvn ${SETTINGS} -Dgpg.keyname=BF1447AC ${PASSPHRASE} clean deploy |grep -v "^Generating"
+mvn ${SETTINGS} -Dgpg.keyname=BF1447AC ${PASSPHRASE} clean deploy
 exit $?
