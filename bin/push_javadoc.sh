163cb7110a1c21925f06f5f8115c8ec5793c1a1e
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-16 11:18:28 +0100

    quiet git stuff

--- bin/push_javadoc.sh
+++ bin/push_javadoc.sh
@@ -4,12 +4,12 @@ REMOTE=$1
 VERSION=$2
 git -C ${LOCAL} init
 git -C ${LOCAL} remote add origin git@github.com:btrplace/${REMOTE} || exit 1
-git -C ${LOCAL} pull origin gh-pages ||exit 1
+git -C ${LOCAL} pull origin gh-pages||exit 1
 git -C ${LOCAL} checkout gh-pages || exit 1
 cd ${LOCAL}
 rm -rf *
 cd -
-git -C ${LOCAL} commit -m "clean" -a || exit 1
+git -C ${LOCAL} commit -m "clean" -a > /dev/null || exit 1
 
 #Generate and copy
 echo "Generate the new javadoc"
@@ -19,7 +19,7 @@ cp -r target/site/apidocs/* ${LOCAL}/
 #Publish
 echo "Publishing"
 cd ${LOCAL}
-git add *|| exit 1
+git add * > /dev/null|| exit 1
 cd -
 git -C ${LOCAL} commit -m "apidoc for version ${VERSION}" -a || exit 1
 git -C ${LOCAL} push origin gh-pages|| exit 1
