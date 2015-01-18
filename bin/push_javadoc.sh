4252258e72578eca92405feb709e0f8440bc874c
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-18 22:51:16 +0100

    - Print usage for push_javadoc.sh - fix #60

--- bin/push_javadoc.sh
+++ bin/push_javadoc.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 remote version"
+	exit 1
+fi
 LOCAL=`mktemp -d -t btrplace.XXX`
 REMOTE=$1
 VERSION=$2
+
 git -C ${LOCAL} init
-git -C ${LOCAL} remote add origin git@github.com:btrplace/${REMOTE} || exit 1
+git -C ${LOCAL} remote add origin git@github.com:btrplace/${REMOTE}||exit 1
 git -C ${LOCAL} pull origin gh-pages||exit 1
-git -C ${LOCAL} checkout gh-pages || exit 1
+git -C ${LOCAL} checkout gh-pages||exit 1
 
 #Generate and copy
-mvn -q compile -DskipTests javadoc:aggregate || exit 1
+mvn -q compile -DskipTests javadoc:aggregate||exit 1
 cp -r target/site/apidocs/* ${LOCAL}/
 
 #Publish
 cd ${LOCAL}
 git add *
 cd -
-git -C ${LOCAL} commit -m "apidoc for version ${VERSION}" -a || exit 1
-git -C ${LOCAL} push origin gh-pages|| exit 1
+git -C ${LOCAL} commit -m "apidoc for version ${VERSION}" -a||exit 1
+git -C ${LOCAL} push origin gh-pages||exit 1
 rm -rf ${LOCAL}
