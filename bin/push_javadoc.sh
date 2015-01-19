f575812c468fc009f606e7b294920cdebe6e4e60
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-01-19 13:53:53 +0100

    first try for the cache

--- bin/push_javadoc.sh
+++ bin/push_javadoc.sh
@@ -24,7 +24,7 @@ cd ${LOCAL}
 rm -rf *
 cd -
 #Generate and copy
-mvn -q compile -DskipTests javadoc:aggregate||exit 1
+mvn -q compile -Dmaven.repo.local=/tmp/cache -DskipTests javadoc:aggregate||exit 1
 cp -r target/site/apidocs/* ${LOCAL}/
 echo ${HEAD} > ${LOCAL}/.commit
 
