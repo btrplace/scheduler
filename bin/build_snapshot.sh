0600665ba5d2a2caee27cbbf30946ab77e1f4773
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-10-13 23:24:10 +0200

    improve caching ?

--- bin/build_snapshot.sh
+++ bin/build_snapshot.sh
@@ -14,9 +14,9 @@ V=$(getVersion)
 if [[ ${V} == *-SNAPSHOT ]]; then
 #    mvn -Dmaven.repo.local=/tmp/cache clean test||exit 1
     mvn clean test||exit 1    
- #   bin/cache.sh push /tmp/cache
+#   bin/cache.sh push /tmp/cache
     ./bin/deploy.sh||quit "Unable to deploy"
- #   bin/cache.sh push /tmp/cache
+#   bin/cache.sh push /tmp/cache
 else
     echo "${V} is not a snapshot version. Exiting"
     exit 1
