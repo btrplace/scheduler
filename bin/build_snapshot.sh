46712b80d6ce5f640ff7a6dbbf6ec8e799b90d1a
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-03-31 08:30:06 +0200

    notes

--- bin/build_snapshot.sh
+++ bin/build_snapshot.sh
@@ -5,7 +5,9 @@ source bin/commons.sh
 V=$(getVersion)
 if [[ ${V} == *-SNAPSHOT ]]; then
     mvn clean test||exit 1
+    echo "** Releasing to sonatype **"
     ./bin/deploy.sh > deploy.out 2>&1 ||err "  Unable to deploy" deploy.out
+    echo "** pushing the javadoc **"
     ./bin/push_javadoc.sh btrplace/apidocs-next.git >javadoc.out 2>&1  || warn "  Unable to push the javadoc" javadoc.out
 else
     echo "${V} is not a snapshot version. Exiting"
