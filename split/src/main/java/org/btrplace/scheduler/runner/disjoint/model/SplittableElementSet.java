e9126fdec7e610ad3f519fa6cf47af53f042107f
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-04-25 21:33:25 +0200

    revert

--- split/src/main/java/org/btrplace/scheduler/runner/disjoint/model/SplittableElementSet.java
+++ split/src/main/java/org/btrplace/scheduler/runner/disjoint/model/SplittableElementSet.java
@@ -50,11 +50,12 @@ public class SplittableElementSet<E extends Element> implements Comparator<E> {
      * @param idx the partition associated to each element. Format {@link org.btrplace.model.Element#id()} -> key
      */
     public SplittableElementSet(Collection<E> c, TIntIntHashMap idx) {
+        values = new ArrayList<>();
+        for (E e : c) {
+            values.add(e);
+        }
         this.index = idx;
-        Set<E> s = new TreeSet<>(this);
-        s.addAll(c);
-        values = new ArrayList<>(s);
-        //Collections.sort(values, this);
+        Collections.sort(values, this);
     }
 
     /**
