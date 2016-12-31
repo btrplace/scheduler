337ab63247e25823df9a8d8785aed522851b269a
Fabien Hermenier <fabien.hermenier@unice.fr>
2016-12-31 11:41:07 +0100

    fix

--- safeplace/src/test/java/org/btrplace/safeplace/spec/SpecScannerTest.java
+++ safeplace/src/test/java/org/btrplace/safeplace/spec/SpecScannerTest.java
@@ -33,7 +33,7 @@ public class SpecScannerTest {
     public void test() throws Exception {
         SpecScanner sc = new SpecScanner();
         List<Constraint> l = sc.scan();
-        Assert.assertEquals(l.size(), 25);
+        Assert.assertEquals(l.size(), 27);
         long from = System.currentTimeMillis();
         System.out.println(l.stream().map(Constraint::pretty).collect(Collectors.joining("\n")));
         System.out.println(System.currentTimeMillis() - from + " ms");
