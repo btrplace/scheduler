671d7052c90052b4a4f94932e7cdb901ca82c2de
Fabien Hermenier <fabien.hermenier@unice.fr>
2017-01-12 15:16:46 -0800

    Add a test to check MinMigration behavior

--- choco/src/test/java/org/btrplace/scheduler/choco/constraint/migration/CMinMigrationsTest.java
+++ choco/src/test/java/org/btrplace/scheduler/choco/constraint/migration/CMinMigrationsTest.java
@@ -19,10 +19,17 @@
 package org.btrplace.scheduler.choco.constraint.migration;
 
 import org.btrplace.json.JSON;
+import org.btrplace.model.DefaultModel;
 import org.btrplace.model.Instance;
+import org.btrplace.model.Model;
+import org.btrplace.model.Node;
+import org.btrplace.model.VM;
 import org.btrplace.model.constraint.MinMTTR;
 import org.btrplace.model.constraint.MinMigrations;
 import org.btrplace.model.constraint.OptConstraint;
+import org.btrplace.model.constraint.Preserve;
+import org.btrplace.model.constraint.SatConstraint;
+import org.btrplace.model.view.ShareableResource;
 import org.btrplace.model.view.network.Network;
 import org.btrplace.plan.ReconfigurationPlan;
 import org.btrplace.plan.event.MigrateVM;
@@ -35,7 +42,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * @author Fabien Hermenier
@@ -58,27 +64,61 @@ public class CMinMigrationsTest {
 
 
     @Test
-    public void testFoo() {
+    public void simpleTest() {
+        Model mo = new DefaultModel();
+        Node n0 = mo.newNode();
+        Node n1 = mo.newNode();
+
+        VM vm0 = mo.newVM();
+        VM vm1 = mo.newVM();
+        VM vm2 = mo.newVM();
+
+        mo.getMapping().on(n0, n1)
+                .run(n0, vm0, vm1, vm2);
+
+        ShareableResource mem = new ShareableResource("mem", 5, 1);
+
+        //cpu dimension to create the violation
+        ShareableResource cpu = new ShareableResource("cpu", 5, 1);
+        mem.setConsumption(vm0, 3); //VM0 as the big VM
+
+        mo.attach(cpu);
+        mo.attach(mem);
+        List<SatConstraint> cstrs = new ArrayList<>();
+        //The 3 VMs no longer feet on n0
+        cstrs.add(new Preserve(vm0, "cpu", 5));
+
+        ChocoScheduler s = new DefaultChocoScheduler();
+        s.doOptimize(true);
+        ReconfigurationPlan p = s.solve(mo, cstrs, new MinMigrations());
+        System.out.println(s.getStatistics());
+        System.out.println(p);
+        Assert.assertEquals(p.getActions().size(), 1); //VM0 to n1
+        Assert.assertEquals(p.getResult().getMapping().getVMLocation(vm0), n1); //VM0 to n1
+    }
+
+    @Test
+    public static void main(String[] args) {
         String root = "/Users/fabien.hermenier/Documents/BtrPlace/nutanix/instances";
         List<OptConstraint> objs = Arrays.asList(new MinMTTR(), new MinMigrations());
-        boolean verbose = false;
+        boolean verbose = true;
 
         for (OptConstraint o : objs) {
             System.out.print(" " + o);
         }
         System.out.println();
-        for (int idx = 2; idx <= 7; idx++) {
+        for (int idx = 7; idx <= 7; idx++) {
             String path = root + "/instance-" + idx + ".json";
             if (verbose) {
                 System.out.println("--- " + idx + " --- ");
+            } else {
+                System.out.print(idx);
             }
 
             List<Long> res = new ArrayList<>();
             for (OptConstraint o : objs) {
                 if (verbose) {
                     System.out.println("\t" + o);
-                } else {
-                    System.out.print(idx);
                 }
                 Instance i = JSON.readInstance(new File(path));
                 if (Network.get(i.getModel()) != null) {
@@ -87,14 +127,14 @@ public class CMinMigrationsTest {
                 i = new Instance(i.getModel(), i.getSatConstraints(), o);
                 ChocoScheduler s = new DefaultChocoScheduler();
                 s.doOptimize(true);
-                s.doRepair(false);
-                s.setTimeLimit(10);
-                //s.setVerbosity(3);
+                s.setTimeLimit(20);
+                s.doRepair(true);
                 ReconfigurationPlan p = s.solve(i);
                 Assert.assertNotNull(p);
                 res.add(p.getActions().stream().filter(x -> x instanceof MigrateVM).count());
                 if (verbose) {
                     System.out.println(s.getStatistics());
+                    System.out.println(p);
                 }
             }
             if (!verbose) {
