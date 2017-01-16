de0e9e6638db617fbdf9f9e957f2901880bac9c3
Fabien Hermenier <fabien.hermenier@unice.fr>
2017-01-15 21:13:56 -0800

    toString() for LinearToAResourceActionDuration

--- choco/src/main/java/org/btrplace/scheduler/choco/duration/LinearToAResourceActionDuration.java
+++ choco/src/main/java/org/btrplace/scheduler/choco/duration/LinearToAResourceActionDuration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 University Nice Sophia Antipolis
+ * Copyright (c) 2017 University Nice Sophia Antipolis
  *
  * This file is part of btrplace.
  * This library is free software; you can redistribute it and/or
@@ -137,5 +137,10 @@ public class LinearToAResourceActionDuration<E extends Element> implements Actio
      */
     public void setOffset(double b) {
         this.offset = b;
+    }
+
+    @Override
+    public String toString() {
+        return coefficient + " x " + rc + " + " + offset;
     }
 }
