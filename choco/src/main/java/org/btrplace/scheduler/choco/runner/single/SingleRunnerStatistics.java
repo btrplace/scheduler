e39095b6abfeaa5e745c7c8a5e6a3128accbd58f
Fabien Hermenier <fabien.hermenier@unice.fr>
2017-11-14 13:40:29 +0100

    close #164

--- choco/src/main/java/org/btrplace/scheduler/choco/runner/single/SingleRunnerStatistics.java
+++ choco/src/main/java/org/btrplace/scheduler/choco/runner/single/SingleRunnerStatistics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 University Nice Sophia Antipolis
+ * Copyright (c) 2017 University Nice Sophia Antipolis
  *
  * This file is part of btrplace.
  * This library is free software; you can redistribute it and/or
@@ -168,9 +168,11 @@ public class SingleRunnerStatistics implements SolvingStatistics {
         }
         b.append("; ").append(nbConstraints).append(" constraint(s)");
 
-        if (params.doOptimize()) {
-            b.append("; optimize");
-        }
+      if (params.doOptimize()) {
+        b.append("; optimize");
+      } else {
+        b.append("; satisfy");
+      }
         if (params.getTimeLimit() > 0) {
             b.append("; timeout: ").append(params.getTimeLimit()).append("s");
         }
